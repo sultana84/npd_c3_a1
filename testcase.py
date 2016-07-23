aade5f0cbbf9d5400dc96944928dbe3fb5f3ed21
sultana84 <sultanaisgneiss@gmail.com>
2016-07-23 20:36:35 +0000

    update assignment

--- testcase.py
+++ testcase.py
@@ -3,12 +3,19 @@ import exercise
 
 class OutcomeTest(unittest.TestCase):
 
-   def test_is_x_values(self):
+   def test_is_x_strings(self):
        x = exercise.Yourdict
        x.a = 'bananas'
        self.assertEqual(x.a, 'bananas')
        x.b = 'apples'
        self.assertNotEqual(x.b, 'cherries')
-       
+
+   def test_is_non_strings(self):
+       x = exercise.Yourdict
+       x.a = 'bananas'
+       x.b = 'apples'
+       self.assertIsNotNone(x.a, '1')
+       self.assertIsNotNone(x.b, '2')
+              
 if __name__ == '__main__':
     unittest.main()
